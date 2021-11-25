c5f702250eba5d73dbcae93155f82e33e54b9656
RemonJoL <83402319+RemonJoL@users.noreply.github.com>
2021-11-25 11:34:16 +0000

    Coding examples update
    - Added buttons to hide and expand code examples to portfolio
    - Styled buttons
    - Wrote JS to add the functionality

--- js/coding-examples-expand.js
+++ js/coding-examples-expand.js
@@ -1,13 +1,27 @@
+// ----------------------------------------------
+// Coding Example Toggle
+// ----------------------------------------------
+
+// Declares the necessary variables
 const codingExampleTitle = document.querySelectorAll(".coding-example-title");
 const codingExampleContent = document.querySelectorAll(".coding-example-content");
+const codingExampleArrow = document.querySelectorAll(".coding-example-expand-container");
+const codingExampleArrowImg = document.querySelectorAll(".coding-example-arrow");
 
-for(let i=0;i<codingExampleTitle.length;i++){
+// Loops through all titles and buttons to add function to every one of them
+for(let i=0;i<codingExampleArrow.length;i++){
+  // Adds following functionality to the arrow button:
+  // 1. changes the orientation of the button when clicked
+  // 2. toggles visibility of the code example
+  codingExampleArrow[i].addEventListener("click", function(){
+      codingExampleArrowImg[i].classList.toggle("arrow-up");
+      codingExampleContent[i].classList.toggle("coding-example-visible");
+  });
+  // Adds following functionality to the code example title:
+  // 1. changes the orientation of the button when clicked
+  // 2. toggles visibility of the code example
   codingExampleTitle[i].addEventListener("click", function(){
-    if (codingExampleContent[i].style.display === "none" ){
-      codingExampleContent[i].style.display = "block";
-    }
-    else if (codingExampleContent[i].style.display = "block" ){
-      codingExampleContent[i].style.display = "none";
-    }
+      codingExampleArrowImg[i].classList.toggle("arrow-up");
+      codingExampleContent[i].classList.toggle("coding-example-visible");
   });
 }
