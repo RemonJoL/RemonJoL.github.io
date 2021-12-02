4e29b23ac7d46e45039f1dd90de975900302d839
RemonJoL <83402319+RemonJoL@users.noreply.github.com>
2021-12-02 15:16:07 +0000

    Feedback update
    - Fixed the back to top button
    - Fixed an issue where on very large screens the main page was stretching to fit, causing it not to look nice, fixed the page and responsiveness but it was a lot more work than expected to get it to behave properly on all screen sizes
    - Put the description of the coding examples at the top of the example, so people know what they'll be looking at before seeing the actual code
    - Fixed the drop down menus for the code examples so it works in IE
    - Fixed text alignment on the about me and SCS Scheme pages, making the paragraphs align left and titles centered

--- js/coding-examples-expand.js
+++ js/coding-examples-expand.js
@@ -8,20 +8,52 @@ const codingExampleContent = document.querySelectorAll(".coding-example-content"
 const codingExampleArrow = document.querySelectorAll(".coding-example-expand-container");
 const codingExampleArrowImg = document.querySelectorAll(".coding-example-arrow");
 
-// Loops through all titles and buttons to add function to every one of them
-for(let i=0;i<codingExampleArrow.length;i++){
+// // Loops through all titles and buttons to add function to every one of them
+// for(let i=0;i<codingExampleArrow.length;i++){
+//   // Adds following functionality to the arrow button:
+//   // 1. changes the orientation of the button when clicked
+//   // 2. toggles visibility of the code example
+//   codingExampleArrow[i].addEventListener("click", function(){
+//       codingExampleArrowImg[i].classList.toggle("arrow-up");
+//       codingExampleContent[i].classList.toggle("coding-example-visible");
+//   });
+//   // Adds following functionality to the code example title:
+//   // 1. changes the orientation of the button when clicked
+//   // 2. toggles visibility of the code example
+//   codingExampleTitle[i].addEventListener("click", function(){
+//       codingExampleArrowImg[i].classList.toggle("arrow-up");
+//       codingExampleContent[i].classList.toggle("coding-example-visible");
+//   });
+// }
+
+var _loop = function _loop(i) {
   // Adds following functionality to the arrow button:
   // 1. changes the orientation of the button when clicked
   // 2. toggles visibility of the code example
-  codingExampleArrow[i].addEventListener("click", function(){
-      codingExampleArrowImg[i].classList.toggle("arrow-up");
-      codingExampleContent[i].classList.toggle("coding-example-visible");
-  });
-  // Adds following functionality to the code example title:
+  codingExampleArrow[i].addEventListener("click", function () {
+    if (codingExampleArrowImg[i].className == "coding-example-arrow") {
+      codingExampleArrowImg[i].className = "coding-example-arrow arrow-up";
+      codingExampleContent[i].className = "coding-example-content coding-example-visible";
+    } else if (codingExampleArrowImg[i].className == "coding-example-arrow arrow-up") {
+      codingExampleArrowImg[i].className = "coding-example-arrow";
+      codingExampleContent[i].className = "coding-example-content";
+    }
+  }); // Adds following functionality to the code example title:
   // 1. changes the orientation of the button when clicked
   // 2. toggles visibility of the code example
-  codingExampleTitle[i].addEventListener("click", function(){
-      codingExampleArrowImg[i].classList.toggle("arrow-up");
-      codingExampleContent[i].classList.toggle("coding-example-visible");
+
+  codingExampleTitle[i].addEventListener("click", function () {
+    if (codingExampleArrowImg[i].className == "coding-example-arrow") {
+      codingExampleArrowImg[i].className = "coding-example-arrow arrow-up";
+      codingExampleContent[i].className = "coding-example-content coding-example-visible";
+    } else if (codingExampleArrowImg[i].className == "coding-example-arrow arrow-up") {
+      codingExampleArrowImg[i].className = "coding-example-arrow";
+      codingExampleContent[i].className = "coding-example-content";
+    }
   });
+};
+
+// Loops through all titles and buttons to add function to every one of them
+for (var i = 0; i < codingExampleArrow.length; i++) {
+  _loop(i);
 }
--- js/email-validation.js
+++ js/email-validation.js
@@ -129,7 +129,6 @@ let greenDotCounter;
 
 // Adds click functionality to submit button
 submitBtn.addEventListener("click", function(){
-  // event.preventDefault();
   // Resets green dot counter to 0
   greenDotCounter = 0;
   // Loops through green dots and checks how many are displayed
@@ -146,6 +145,7 @@ submitBtn.addEventListener("click", function(){
     setTimeout(function(){
       confirmation.style.display = "none";
     }, 2000);
+    event.preventDefault();
   }
   else {
     // Displays popup message saying valid information has to be entered, then hides popup
@@ -154,5 +154,6 @@ submitBtn.addEventListener("click", function(){
     setTimeout(function(){
       confirmation.style.display = "none";
     }, 2000);
+    event.preventDefault();
   }
 });
